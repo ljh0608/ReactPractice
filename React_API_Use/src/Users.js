f53660444ae5bce1cfeda33fdf4b1f509b0e54b6
ljh0608 <m98m@naver.com>
2023-01-11 16:03:52 +0900

    Users reducer

--- React_API_Use/src/Users.js
+++ React_API_Use/src/Users.js
@@ -1,33 +1,58 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useReducer } from 'react';
 import axios from 'axios';
 
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'LOADING':
+            return {
+                loading: true,
+                data: null,
+                error: null,
+            };
+        case 'SUCCESS':
+            return {
+                loading: false,
+                data: action.data,
+                error: null,
+            };
+        case 'ERROR':
+            return {
+                loading: false,
+                data: null,
+                error: action.error,
+            };
+        default:
+            throw new Error(`Unhandled action type: ${action.type}`);
+    }
+};
+
 const Users = () => {
-    const [users, setUsers] = useState(null);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(null);
+    const [state, dispatch] = useReducer(reducer, {
+        loading: false,
+        data: null,
+        error: null,
+    });
+
+    const fetchUsers = async () => {
+        dispatch({ type: 'LOADING' });
+
+        try {
+            const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+            dispatch({ type: 'SUCCESS', data: response.data });
+        } catch (e) {
+            dispatch({ type: 'ERROR', error: e });
+        }
+    };
 
     useEffect(() => {
-        const fetchUsers = async () => {
-            try {
-                // 요청이 시작할 때에는 error와 user를 초기화하고 loading의 상태를 true로 바꾼다.
-                setError(null);
-                setUsers(null);
-                setLoading(true);
-                const response = await axios.get('https://jsonplaceholder.typicode.com/users');
-                setUsers(response.data); //data는 response 안에 들어있다.
-                console.log(response.data);
-            } catch (e) {
-                setError(e);
-            }
-            setLoading(false);
-        };
         fetchUsers();
     }, []);
 
-    if (loading) return <div>로딩중 ...</div>;
-    if (error) return <div>에러 발생</div>;
-    if (!users) return null;
+    const { loading, data: users, error } = state; //state.data 를 users키워드로 조회가능
 
+    if (loading) return <div>로딩중..</div>;
+    if (error) return <div>에러가 발생했습니다</div>;
+    if (!users) return null;
     return (
         <div>
             <ul>
@@ -37,35 +62,10 @@ const Users = () => {
                     </li>
                 ))}
             </ul>
+            <div>
+                <button onClick={fetchUsers}>다시 불러오기</button>
+            </div>
         </div>
     );
 };
 export default Users;
-
-// import { useReducer, useEffect, useCallback } from 'react';
-
-// const reducer = (state, action) => {
-//     switch (action.type) {
-//         case 'LOADING':
-//             return {
-//                 loading: true,
-//                 data: null,
-//                 error: null,
-//             };
-//         case 'SUCCESE':
-//             return {
-//                 loading: false,
-//                 data: action.data,
-//                 error: null,
-//             };
-//         case 'ERROR':
-//             return {
-//                 loading: false,
-//                 data: null,
-//                 error: action.error,
-//             };
-//         default:
-//             throw new Error(`Unhandled action type: ${action.type}`);
-//     }
-
-// };
