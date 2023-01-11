9fb94373c425956d401cfb0a4113926e935cbc0c
ljh0608 <m98m@naver.com>
2023-01-11 17:47:47 +0900

    COSTOM HOOK useAsync

--- React_API_Use/src/Users.js
+++ React_API_Use/src/Users.js
@@ -1,70 +1,36 @@
-import React, { useEffect, useReducer } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import useAsync from './useAsync';
+import User from './User';
 
-const reducer = (state, action) => {
-    switch (action.type) {
-        case 'LOADING':
-            return {
-                loading: true,
-                data: null,
-                error: null,
-            };
-        case 'SUCCESS':
-            return {
-                loading: false,
-                data: action.data,
-                error: null,
-            };
-        case 'ERROR':
-            return {
-                loading: false,
-                data: null,
-                error: action.error,
-            };
-        default:
-            throw new Error(`Unhandled action type: ${action.type}`);
-    }
+const getUsers = async () => {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+    return response.data;
 };
 
 const Users = () => {
-    const [state, dispatch] = useReducer(reducer, {
-        loading: false,
-        data: null,
-        error: null,
-    });
+    const [userId, setUserId] = useState(null);
+    const [state, refetch] = useAsync(getUsers, [], true);
+    const { loading, data: users, error } = state;
 
-    const fetchUsers = async () => {
-        dispatch({ type: 'LOADING' });
-
-        try {
-            const response = await axios.get('https://jsonplaceholder.typicode.com/users');
-            dispatch({ type: 'SUCCESS', data: response.data });
-        } catch (e) {
-            dispatch({ type: 'ERROR', error: e });
-        }
-    };
-
-    useEffect(() => {
-        fetchUsers();
-    }, []);
-
-    const { loading, data: users, error } = state; //state.data 를 users키워드로 조회가능
+    useEffect(() => {}, []);
 
     if (loading) return <div>로딩중..</div>;
     if (error) return <div>에러가 발생했습니다</div>;
-    if (!users) return null;
+    if (!users) return <button onClick={refetch}>불러오기</button>;
     return (
         <div>
             <ul>
                 {users.map((user) => (
-                    <li key={user.id}>
+                    <li key={user.id} onClick={() => setUserId(user.id)} style={{ cursor: 'pointer' }}>
                         {user.username} ({user.name})
                     </li>
                 ))}
             </ul>
             <div>
-                <button onClick={fetchUsers}>다시 불러오기</button>
+                <button onClick={refetch}>다시 불러오기</button>
             </div>
+            {userId && <User id={userId} />}
         </div>
     );
 };
