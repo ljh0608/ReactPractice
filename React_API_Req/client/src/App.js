0d410b49bac2cd5efcef3057eb1e21e722ea35af
ljh0608 <m98m@catholic.ac.kr>
2023-03-05 16:47:51 +0900

    axios 서버 통신

--- React_API_Req/client/src/App.js
+++ React_API_Req/client/src/App.js
@@ -1,34 +1,39 @@
 import './App.css';
 import { useState } from 'react';
 import { useEffect } from 'react';
+import axios from 'axios';
 function App() {
     const [todoList, setTodoList] = useState(null);
+    const SERVER_URL = 'http://localhost:3002/api/todo';
+    const fetchData = async () => {
+        const response = await axios.get(SERVER_URL);
+        setTodoList(response.data);
 
-    const fetchData = () => {
-        fetch('http://localhost:3002/api/todo')
-            .then((response) => response.json())
-            .then((data) => setTodoList(data));
+        // fetch('http://localhost:3002/api/todo')
+        //     .then((response) => response.json())
+        //     .then((data) => setTodoList(data));
     };
 
     useEffect(() => {
         fetchData();
     }, []);
 
-    const onSubmithandler = (e) => {
+    const onSubmithandler = async (e) => {
         const text = e.target.text.value;
         const done = e.target.done.checked;
         e.preventDefault();
-
-        fetch('http://localhost:3002/api/todo', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                text,
-                done,
-            }),
-        }).then(() => fetchData());
+        await axios.post(SERVER_URL, { text, done });
+        fetchData();
+        // fetch('http://localhost:3002/api/todo', {
+        //     method: 'POST',
+        //     headers: {
+        //         'Content-Type': 'application/json',
+        //     },
+        //     body: JSON.stringify({
+        //         text,
+        //         done,
+        //     }),
+        // }).then(() => fetchData());
     };
     return (
         <div className="App">
